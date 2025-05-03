d811fe44dc6ea6639bfc85ad6e292854c46ebcdc
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-05-03 13:24:16 +0530

    adding it from thr datCamera.js

--- src/data/camera.js
+++ src/data/camera.js
@@ -1,23 +1,27 @@
 export const cameraData = [
   {
+    author: "Gafoor.TC",
     title: "Uho-P2A-M3F4D",
     description:
       "High quality image with 3MP, 1/3,2304*1296@20fps in the main stream,Built-in Mic & Speaker, support two-way audio, offer better interaction,Support sound and light warning, makes warning and alarming more noticeable",
     image: "/pictures/Uho-P2A-M3F4D.png",
   },
   {
+    author: "Shihabb.p",
     title: "Uho-P3C-M5F4",
     description:
       "High quality image with 5MP, 1/1.8,5MP (2880*1620)@25/20fps; 4MP (2560*1440)@25/20fps; 3MP (2304*1296)@30/25fps; 2MP (1920*1080)@30/25fps;H.264,-Colorhunter%20with%20Wise,Colorhunter with Wise-ISP technology ensures 24-hour full-color images,Support digital WDR (Wide Dynamic Range)",
     image: "/pictures/Uho-P3C-M5F4.png",
   },
   {
+    author: "Roshan Aqther",
     title: "Uho-S3S-M33D",
     description:
       "Fixed camera and PT camera integration, dual channels with one IP, look at overall situation and consider details at the same time,Ultra 265, H.265, H.264,Support digital WDR (Wide Dynamic Range),Built-in Mic & Speaker, support two-way audio, offer better interaction",
     image: "/pictures/Uho-S3S-M33D.png",
   },
-  {
+  {   
+     author: "Zameel",
     title: "Uho-B2D-M5F3D",
     description:
       "High quality image with 5MP, 1/3,Ultra 265, H.265, H.264, MJPEG,Smart IR, up to 20m (66ft) IR distance,Support 128 G Micro SD card",
